9bb6392406621d5f5f4225faf726a97957280b86
Ravindra Patle <me24tech@gmail.com>
2021-06-21 01:03:17 +0530

    Updated the Login page errors with error-message component

--- src/app/components/shared.modules.ts
+++ src/app/components/shared.modules.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { ErrorMessageComponent } from './error-message/error-message.component';
 import { PickupCallCardComponent } from './pickup-call-card/pickup-call-card.component';
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  exports: [PickupCallCardComponent],
-  declarations: [PickupCallCardComponent],
+  exports: [PickupCallCardComponent, ErrorMessageComponent],
+  declarations: [PickupCallCardComponent, ErrorMessageComponent],
 })
 export class SharedModules {}
